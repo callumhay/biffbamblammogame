46640b9725f950e60715aeada5382c41b0f38d70
callumhay <callumhay@f2063ffc-9a4d-0410-8d14-35da9c29b421>
2009-07-18 23:47:20 +0000

    Moved window management into separate class.

---
+++ trunk/WindowManager.cpp
@@ -0,0 +1,105 @@
+#include "WindowManager.h"
+
+WindowManager* WindowManager::instance = NULL;
+
+const char* WindowManager::WINDOW_TITLE								= "Biff! Bam!! Blammo!?!";
+const char* WindowManager::ICON_FILEPATH							= "BiffBamBlammoIcon.bmp";
+const unsigned int WindowManager::DEFAULT_VIDEO_FLAGS = SDL_OPENGL | SDL_SWSURFACE;
+
+WindowManager::WindowManager() : videoSurface(NULL), bitsPerPixel(16) {
+}
+
+WindowManager::~WindowManager() {
+	this->Shutdown();
+}
+
+/**
+ * Initializes the draw window for the game - this will establish
+ * the entire video/window system for drawing everything at the given resolution.
+ */
+bool WindowManager::Init(int width, int height, bool isFullscreen) {
+	// Make sure any previous SDL initialization is not active
+	if (this->videoSurface != NULL) {
+		assert(false);
+		return false;
+	}
+
+	// Load SDL, make sure the window is centered
+	putenv("SDL_VIDEO_CENTERED=1");
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    debug_output("Unable to initialize SDL: " << SDL_GetError());
+		std::cerr << "Unable to initialize SDL: " << SDL_GetError();
+    return false;
+  }
+	
+	// Don't show the mouse cursor unless we're in debug mode
+#ifdef _DEBUG
+	SDL_ShowCursor(1);
+#else
+	SDL_ShowCursor(0);
+#endif
+
+	SDL_WM_SetCaption(WindowManager::WINDOW_TITLE, WindowManager::WINDOW_TITLE);
+
+  // To use OpenGL, you need to get some information first,
+  const SDL_VideoInfo *info = SDL_GetVideoInfo();
+  if (!info) {
+    // This should never happen...
+		debug_output("Video query failed: " << SDL_GetError() << std::endl);
+		std::cerr << "Video query failed: " << SDL_GetError() << std::endl;
+    return false;
+  }
+  this->bitsPerPixel = info->vfmt->BitsPerPixel;
+
+  // Set colour bits...
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE,   8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,  8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+  
+	// Set colour depth
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+  // Double buffering
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	// .. OTHER STUFF HERE
+
+	// Multisampling ?
+	//SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+	//SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, GameDisplay::NUM_MULTISAMPLES);
+
+  // Set the draw surface...
+	unsigned int videoFlags = WindowManager::DEFAULT_VIDEO_FLAGS | (isFullscreen ? SDL_FULLSCREEN : NULL);
+	this->videoSurface = SDL_SetVideoMode(width, height, bitsPerPixel, videoFlags);
+	if (this->videoSurface == NULL) {
+		debug_output("Unable to set video mode: " << SDL_GetError());
+		std::cerr << "Unable to set video mode: " << SDL_GetError();
+    return false;
+  }
+	
+	SDL_Surface* icon = SDL_LoadBMP(WindowManager::ICON_FILEPATH);
+	SDL_SetColorKey(icon, SDL_SRCCOLORKEY, SDL_MapRGB(icon->format, 255, 0, 255));
+	SDL_WM_SetIcon(icon, NULL);
+
+	return true;
+}
+
+/**
+ * Shut down the windowing system. Call this before calling Init again.
+ */
+void WindowManager::Shutdown() {
+	SDL_Quit();									// This will obliterate all surfaces and other SDL objects
+	this->videoSurface = NULL;	// To tell this object that video is dead
+}
+
+/**
+ * Toggles fullscreen mode on/off for the render window.
+ */
+bool WindowManager::ToggleFullscreen() {
+	if (this->videoSurface == NULL) {
+		assert(false);
+		return false;
+	}
+	
+	int success = SDL_WM_ToggleFullScreen(this->videoSurface);
+	return (success == 1);
+}
---
+++ trunk/WindowManager.h
@@ -0,0 +1,61 @@
+/**
+ * WindowManager.h
+ *
+ * (cc) Creative Commons Attribution-Noncommercial-Share Alike 2.5 Licence
+ * Callum Hay, 2009
+ *
+ * You may not use this work for commercial purposes.
+ * If you alter, transform, or build upon this work, you may distribute the 
+ * resulting work only under the same or similar licence to this one.
+ */
+
+#ifndef __WINDOWMANAGER_H__
+#define __WINDOWMANAGER_H__
+
+#include "BlammoEngine/BasicIncludes.h"
+
+/**
+ * Window manager singleton class - this class manages the windowing
+ * system used by the game. Acts as a abstraction away from whatever
+ * implementation is being used and provides only the functionality
+ * needed for the game.
+ */
+class WindowManager {
+
+public:
+	static WindowManager* GetInstance() {
+		if (WindowManager::instance == NULL) {
+			WindowManager::instance = new WindowManager();
+		}
+		return WindowManager::instance;
+	}
+
+	static void DeleteInstance() {
+		if (WindowManager::instance != NULL) {
+			delete WindowManager::instance;
+			WindowManager::instance = NULL;
+		}
+	}
+
+	bool Init(int width, int height, bool isFullscreen);
+	void Shutdown();
+
+	bool ToggleFullscreen();
+
+private:
+	static WindowManager* instance;
+
+	// Constants used to setup the window
+	static const char* WINDOW_TITLE;
+	static const char* ICON_FILEPATH;
+	static const unsigned int DEFAULT_VIDEO_FLAGS;
+	
+	SDL_Surface* videoSurface;
+	int bitsPerPixel;
+	
+
+	WindowManager();
+	~WindowManager();
+
+};
+#endif
