0c31d201d08e459afe96947ba7129c75a1746df2
callumhay@gmail.com <callumhay@gmail.com@f2063ffc-9a4d-0410-8d14-35da9c29b421>
2014-02-26 20:17:03 +0000

    Added the code license and a python script for updating the code license across .h and .cpp files for BBB.

---
+++ trunk/code_license_replacer.py
@@ -0,0 +1,39 @@
+
+import os
+import re
+import glob
+
+allFileNames  = glob.glob("*.h") + glob.glob("*.cpp")
+allFileNames += glob.glob("BlammoEngine/*.h") + glob.glob("BlammoEngine/*.cpp")
+allFileNames += glob.glob("ESPEngine/*.h") + glob.glob("ESPEngine/*.cpp")
+allFileNames += glob.glob("GameModel/*.h") + glob.glob("GameModel/*.cpp")
+allFileNames += glob.glob("GameView/*.h") + glob.glob("GameView/*.cpp")
+allFileNames += glob.glob("GameControl/*.h") + glob.glob("GameControl/*.cpp")
+
+# Get the license Info...
+licenseTextLines = None
+with open("./code_license.txt") as licenseFile:
+	licenseTextLines = licenseFile.readlines()
+
+if licenseTextLines is None:
+	print "Failed to find and open code_license.txt in script directory."
+	exit(-1)
+
+# Clean up the license text in preparation for placing it into the code as a comment...
+licenseText = ""
+for licenseTextLine in licenseTextLines:
+	licenseText += " * " + licenseTextLine.strip() + "\n"
+
+
+for fileName in allFileNames:
+	with open(fileName, 'r+') as file:
+		fileText = file.read()
+		modifiedFileText = re.sub(r"^\W*/\*\*(.+?)\*/", "/**\n * " + os.path.basename(fileName) + "\n * \n" + licenseText + " */", fileText, count=1, flags=re.DOTALL)
+		
+		file.seek(0)
+		file.write(modifiedFileText)
+		file.truncate()
+		file.close()
+		
+		exit()
+		
