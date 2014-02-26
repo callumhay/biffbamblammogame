e2a5f60d0a858267ddb85745672a3f1b453fec13
callumhay@gmail.com <callumhay@gmail.com@f2063ffc-9a4d-0410-8d14-35da9c29b421>
2014-02-26 20:57:16 +0000

    Updated licensing across all code files.

--- trunk/GameView/CgFxPostRefract.h
+++ trunk/GameView/CgFxPostRefract.h
@@ -1,12 +1,30 @@
 /**
  * CgFxPostRefract.h
- *
- * (cc) Creative Commons Attribution-Noncommercial 3.0 License
- * Callum Hay, 2010-2011
- *
- * You may not use this work for commercial purposes.
- * If you alter, transform, or build upon this work, you may distribute the 
- * resulting work only under the same or similar license to this one.
+ * 
+ * Copyright (c) 2014, Callum Hay
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The names of its contributors may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CALLUM HAY BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef __CGFXPOSTREFRACT_H__
