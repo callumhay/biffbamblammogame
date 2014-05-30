6d8217c5c5caa3f5c71b8d37606a4674d8113578
callumhay@gmail.com <callumhay@gmail.com@f2063ffc-9a4d-0410-8d14-35da9c29b421>
2014-05-30 21:06:06 +0000

    Added the beginnings of a tool for visually generating emitters, sprites, and particles (ESP) -- this will eventually serialize the data import/export it and then be directly consumed by the BlammoEngine and ESPEngine in the game.

---
+++ trunk/ESPDesignerTool/RenderViewWidget.cpp
@@ -0,0 +1,73 @@
+/**
+ * RenderViewWidget.cpp
+ * 
+ * Copyright (c) 2014, Callum Hay
+ * All rights reserved.
+ * 
+ * Redistribution and use of the Biff! Bam!! Blammo!?! code or any derivative
+ * works are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The names of its contributors may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 4. Redistributions may not be sold, nor may they be used in a commercial
+ * product or activity without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CALLUM HAY BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+
+#include "RenderViewWidget.h"
+
+RenderViewWidget::RenderViewWidget(const QGLFormat& format, QWidget *parent) : QGLWidget(format, parent) {
+}
+
+RenderViewWidget::~RenderViewWidget() {
+}
+
+
+void RenderViewWidget::initializeGL() {
+
+}
+
+void RenderViewWidget::paintGL() {
+
+}
+
+void RenderViewWidget::resizeGL(int width, int height) {
+
+}
+
+void RenderViewWidget::resizeOverlayGL(int width, int height) {
+
+}
+
+void RenderViewWidget::mousePressEvent(QMouseEvent* mouseEvent) {
+
+}
+
+void RenderViewWidget::mouseMoveEvent(QMouseEvent* mouseEvent) {
+
+}
+
+void RenderViewWidget::wheelEvent(QWheelEvent* wheelEvent) {
+
+}
+
+void RenderViewWidget::keyPressEvent(QKeyEvent *keyEvent) {
+
+}
